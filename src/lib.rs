6cc22aa1f90a0c65433ed15059689aa8bba02ea0
houru <houruxuan01@gmail.com>
2025-10-31 00:06:16 +1100

    (feat) implement get index, value_on and value_at functions for tso

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,58 @@
 use pyo3::prelude::*;
-use std::vec::Vec;
+use pyo3::exceptions::PyIndexError;
+
 
 #[pyclass]
 struct TimeSeriesObject {
-    keys: Vec<i32>,
+    timestamps: Vec<i32>,
     values: Vec<Py<PyAny>>,
 }
+
 
 #[pymethods]
 impl TimeSeriesObject {
     #[new]
     fn new() -> Self {
-        TimeSeriesObject {keys: Vec::new(), values: Vec::new()}
+        TimeSeriesObject {timestamps: Vec::new(), values: Vec::new()}
     }
 
-    fn add(&mut self, key: i32, value: Py<PyAny>) {
-        self.keys.push(key);
-        self.values.push(value);
+    fn get_insertion_index(&self, ts: i32) -> usize {
+        self.timestamps.binary_search(&ts).unwrap_or_else(|idx| idx)
     }
 
-    fn get(&self, key: i32) -> &Py<PyAny> {
-        let value = &self.values[key as usize];
-        value
+    fn add(&mut self, ts: i32, value: Py<PyAny>) {
+        if self.timestamps.is_empty() || (ts > self.timestamps[self.timestamps.len()-1]) {
+            self.timestamps.push(ts);
+            self.values.push(value);
+        } else {
+            let idx = self.get_insertion_index(ts);
+            self.timestamps.insert(idx, ts);
+            self.values.insert(idx, value);
+        }
+    }
+
+    fn value_at(&self, ts: i32) -> &Py<PyAny> {
+        let idx = self.get_insertion_index(ts);
+        if self.timestamps[idx] == ts {
+            &self.values[idx]
+        } else if idx != 0 {
+            &self.values[idx - 1]
+        } else {
+            &self.values[idx]
+        }
+    }
+
+    fn value_on(&self, ts: i32) -> PyResult<&Py<PyAny>> {
+        let idx = self.get_insertion_index(ts);
+        if self.timestamps[idx] == ts {
+            Ok(&self.values[idx])
+        } else {
+            let err_str = format!("No value found at timestamp: {}", ts);
+             Err(PyIndexError::new_err(err_str))
+        }
     }
 }
+
 
 #[pymodule]
 fn rust_time_series_objects(m: &Bound<'_, PyModule>) -> PyResult<()> {
